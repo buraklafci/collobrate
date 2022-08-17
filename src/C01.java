edd7ba146cdd022d6a88adea3dc0822e61eae07b
OgzSltkBgr <osbtoklucu@gmail.com>
2022-08-17 17:53:27 +0300

    1708

--- src/C01.java
+++ src/C01.java
@@ -2,6 +2,7 @@ public class C01 {
     public static void main(String[] args) {
         System.out.println("colobration");
         System.out.println("colobration cihad");
+        System.out.println("colobration Oguz");
     }
 
 }

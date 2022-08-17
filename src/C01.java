76994c60edc169b5f364f3b942fc2b82a0f4d052
BURAK LAFCI <karanfilkaranfil822@gmail.com>
2022-08-17 17:49:36 +0300

    v5

--- src/C01.java
+++ src/C01.java
@@ -2,5 +2,6 @@ public class C01 {
     public static void main(String[] args) {
         System.out.println("colobration");
         System.out.println("colobration2");
+        System.out.println("colobration3");
     }
 }
